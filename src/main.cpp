487cf46d787a5385b8144309328b75209fa20826
nguyelt1012 <lenguyetds@gmail.com>
2023-02-19 18:34:02 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,27 @@
 
 #define LED1 15
 #define LED2 2
-
+#define BUT1 19
+#define BUT2 21
 String ledStatus1 = "OFF";
 String ledStatus2 = "OFF";
 // Replace with your network credentials
-const char *ssid = "WFKL";
-const char *password = "71759919kl";
+const char *ssid = "POCO M3";
+const char *password = "12345678";
 
-//mqtt broker
-const char *led1Id = "led001";
-const char *led2Id = "led002";
+// mqtt broker
+const char *led1Id = "63d4fc57d977b7b5ec8076c0";
+const char *led2Id = "63edd40aa0dbe4ad7f8408cc";
+volatile byte state1 = HIGH;
+volatile byte state2 = HIGH;
 
 StaticJsonDocument<256> sendingStatusLed;
 
 #define MQTT_SERVER "broker.hivemq.com"
 #define MQTT_PORT 1883
 
-#define MQTT_LED_TOPIC_SUB "mqtt_esp32/smart_home"
-#define MQTT_LED_TOPIC_PUB "mqtt_esp32/led2"
-
+#define MQTT_LED_TOPIC_SUB "iot/group8/control"
+#define MQTT_LED_TOPIC_PUB "iot/group8/data"
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -79,53 +81,34 @@ void callback(char *topic, byte *payload, unsigned int length)
     StaticJsonDocument<256> doc;
     deserializeJson(doc, payload);
 
-    if (doc["value"] == "OFF"){
-        if (strcmp(doc["deviceId"], led1Id) == 0 ){
+    if (doc["control"]["status"] == "OFF")
+    {
+        if (strcmp(doc["deviceId"], led1Id) == 0)
+        {
             ledStatus1 == "OFF";
             digitalWrite(LED1, LOW);
             Serial.println("LED1 OFF");
-            //publish led1
-            // sendingStatusLed["value"] = ledStatus1;
-            // sendingStatusLed["deviceId"] = led1Id;
-            // char out[256];
-            // serializeJson(sendingStatusLed, out);
-            // client.publish(MQTT_LED_TOPIC_PUB, out, true);
         }
-        if (strcmp(doc["deviceId"], led2Id) == 0){
+        if (strcmp(doc["deviceId"], led2Id) == 0)
+        {
             ledStatus2 == "OFF";
             digitalWrite(LED2, LOW);
             Serial.println("LED2 OFF");
-            // sendingStatusLed["roomId"] = roomId1;
-            sendingStatusLed["value"] = ledStatus2;
-            sendingStatusLed["deviceId"] = led2Id;
-            char out[256];
-            serializeJson(sendingStatusLed, out);
-            client.publish(MQTT_LED_TOPIC_PUB, out, true);
-
         }
-    }else if (doc["value"] == "ON"){
-        if (strcmp(doc["deviceId"], led1Id) == 0){
+    }
+    else if (doc["control"]["status"] == "ON")
+    {
+        if (strcmp(doc["deviceId"], led1Id) == 0)
+        {
             ledStatus1 == "ON";
             digitalWrite(LED1, HIGH);
             Serial.println("LED1 ON");
-            //publish led1
-            // sendingStatusLed["value"] = ledStatus1;
-            // sendingStatusLed["deviceId"] = led1Id;
-            // char out[256];
-            // serializeJson(sendingStatusLed, out);
-            // client.publish(MQTT_LED_TOPIC_PUB, out, true);
         }
-        if (strcmp(doc["deviceId"], led2Id) == 0){
-            ledStatus2== "ON";
+        if (strcmp(doc["deviceId"], led2Id) == 0)
+        {
+            ledStatus2 == "ON";
             digitalWrite(LED2, HIGH);
             Serial.println("LED2 ON");
-
-            // sendingStatusLed["roomId"] = roomId1;
-            sendingStatusLed["value"] = ledStatus2;
-            sendingStatusLed["deviceId"] = led2Id;
-            char out[256];
-            serializeJson(sendingStatusLed, out);
-            client.publish(MQTT_LED_TOPIC_PUB, out, true);
         }
     }
 }
@@ -140,15 +123,47 @@ void setup()
     Serial.println("Start transfer");
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
+    pinMode(BUT1, INPUT_PULLUP);
+    pinMode(BUT2, INPUT_PULLUP);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
 }
 
-void loop(){
+void loop()
+{
     client.loop();
     if (!client.connected())
     {
         connect_to_broker();
     }
-    
+    if (digitalRead(BUT1) == 0)
+    {
+        Serial.print(digitalRead(BUT1));
+        while (digitalRead(BUT1) == 0){
+            /* cho nut dc nha */
+        }
+        ledStatus1 = ledStatus1 == "ON" ? "OFF" : "ON";
+        state1 = ledStatus1 == "ON" ? HIGH : LOW;
+        digitalWrite(LED1, state1);
+        sendingStatusLed["control"]["status"] = ledStatus1;
+        sendingStatusLed["deviceId"] = led1Id;
+        char out[256];
+        serializeJson(sendingStatusLed, out);
+        client.publish(MQTT_LED_TOPIC_PUB, out, true);
+    }
+    if (digitalRead(BUT2) == 0)
+    {
+        Serial.print(digitalRead(BUT2));
+        while (digitalRead(BUT2) == 0){
+            /* cho nut dc nha */
+        }
+        ledStatus2 = ledStatus2 == "ON" ? "OFF" : "ON";
+        state2 = ledStatus2 == "ON" ? HIGH : LOW;
+        digitalWrite(LED2, state2);
+        sendingStatusLed["control"]["status"] = ledStatus2;
+        sendingStatusLed["deviceId"] = led2Id;
+        char out[256];
+        serializeJson(sendingStatusLed, out);
+        client.publish(MQTT_LED_TOPIC_PUB, out, true);
+    }
 }
